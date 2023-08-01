af954af1c535cd6a5a88abc144c88123360a4810
Fyodor Danilov <danilovfa@protonmail.com>
2023-08-01 14:50:31 +0300

    Add details mvp

--- app/src/main/java/com/danilovfa/space/presentation/mvp/photo/PhotoPresenter.kt
+++ app/src/main/java/com/danilovfa/space/presentation/mvp/photo/PhotoPresenter.kt
@@ -1,14 +1,26 @@
 package com.danilovfa.space.presentation.mvp.photo
 
+import android.util.Log
+import com.danilovfa.common.domain.usecase.HasTutorialBeenShownUseCase
 import com.github.terrakok.cicerone.Router
 import moxy.InjectViewState
 import moxy.MvpPresenter
-import javax.inject.Inject
 
 @InjectViewState
 class PhotoPresenter(
-    private val router: Router
+    private val router: Router,
+    private val hasTutorialBeenShownUseCase: HasTutorialBeenShownUseCase
 ) : MvpPresenter<PhotoView>() {
+
+    fun doShowTutorial() {
+        val doShow = hasTutorialBeenShownUseCase.execute()
+        Log.d("MySharedPrefs", "doShowTutorial: $doShow")
+        if (doShow) {
+            Log.d("MySharedPrefs", "doShowTutorial: doShow")
+            viewState.showTutorial()
+        }
+    }
+
     fun onBackPressed() {
         router.exit()
     }
