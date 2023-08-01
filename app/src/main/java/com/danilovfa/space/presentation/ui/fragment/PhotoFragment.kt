213b4792a82d50eccb379da3433c89ca6ae1a17a
Fyodor Danilov <danilovfa@protonmail.com>
2023-08-01 15:36:38 +0300

    Add photo details screen

--- app/src/main/java/com/danilovfa/space/presentation/ui/fragment/PhotoFragment.kt
+++ app/src/main/java/com/danilovfa/space/presentation/ui/fragment/PhotoFragment.kt
@@ -1,5 +1,7 @@
 package com.danilovfa.space.presentation.ui.fragment
 
+import android.content.ClipData
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
@@ -12,7 +14,11 @@ import com.danilovfa.space.databinding.FragmentPhotoBinding
 import com.danilovfa.space.presentation.mvp.photo.PhotoPresenter
 import com.danilovfa.space.presentation.mvp.photo.PhotoView
 import com.danilovfa.space.presentation.navigation.BackButtonListener
+import com.danilovfa.space.presentation.ui.dialog.TextDialogFragment
 import com.danilovfa.space.presentation.ui.dialog.TutorialDialogFragment
+import com.danilovfa.space.utils.FileManager
+import com.danilovfa.space.utils.extensions.loadImageByUrl
+import com.danilovfa.space.utils.extensions.loadImageToBitmap
 import dagger.hilt.android.AndroidEntryPoint
 import moxy.presenter.InjectPresenter
 import moxy.presenter.ProvidePresenter
@@ -31,12 +37,29 @@ class PhotoFragment : BaseFragment<FragmentPhotoBinding>(FragmentPhotoBinding::i
     @ProvidePresenter
     fun providePresenter() = hiltPresenter
 
+    private var photoUrl: String? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val menuHost = requireActivity()
         menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         setupToolbar()
+        showPhoto()
+        presenter.doShowTutorial()
+    }
+
+    private fun showPhoto() {
+        photoUrl = requireArguments().getString(EXTRA_IMAGE_URL)
+        if (photoUrl != null) {
+            requireContext().loadImageByUrl(
+                imageUrl = photoUrl!!,
+                view = binding.photoTouchImageView,
+                placeholder = R.drawable.progress_bar_photo_details
+            )
+        } else {
+            showError()
+        }
     }
 
     override fun onHiddenChanged(hidden: Boolean) {
@@ -49,6 +72,36 @@ class PhotoFragment : BaseFragment<FragmentPhotoBinding>(FragmentPhotoBinding::i
         showAppBar()
 
         toolbarShowBackButton {
+            onBackPressed()
+        }
+    }
+
+    private fun sharePhoto() {
+        if (photoUrl != null) {
+            requireContext().loadImageToBitmap(photoUrl!!) { photoBitmap ->
+                val fileManager = FileManager(requireContext())
+                val imageUri = fileManager.saveTempImageBitmap(photoBitmap)
+
+                val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                    clipData = ClipData.newRawUri(null, imageUri)
+                    type = "image/jpeg"
+                    putExtra(Intent.EXTRA_STREAM, imageUri)
+                    flags += Intent.FLAG_GRANT_READ_URI_PERMISSION
+                }
+
+                startActivity(Intent.createChooser(shareIntent, null))
+            }
+        } else {
+            showError()
+        }
+    }
+
+    private fun showError() {
+        TextDialogFragment.display(
+            fragmentManager = childFragmentManager,
+            title = getString(R.string.error_title),
+            body = getString(R.string.error_default)
+        ) {
             onBackPressed()
         }
     }
@@ -76,7 +129,7 @@ class PhotoFragment : BaseFragment<FragmentPhotoBinding>(FragmentPhotoBinding::i
         menuInflater.inflate(R.menu.menu_toolbar_details, menu)
         val menuShare = menu.findItem(R.id.menuShare)
         menuShare.actionView?.setOnClickListener {
-
+            sharePhoto()
         }
     }
 
