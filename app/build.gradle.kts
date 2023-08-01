c26fbafe5da0f69dad28d572fdb3a21e8100fae4
Fyodor Danilov <danilovfa@protonmail.com>
2023-08-01 14:46:32 +0300

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,11 @@ dependencies {
     // Cicerone navigation
     implementation("com.github.terrakok:cicerone:7.1")
 
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.window:window:1.1.0")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    implementation("com.github.MikeOrtiz:TouchImageView:3.4")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
