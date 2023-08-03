5d3c11053a54d255673b52dde1ad2290348396bf
Fyodor Danilov <danilovfa@protonmail.com>
2023-08-03 17:45:03 +0300

    Add map screen MVP

--- app/src/main/java/com/danilovfa/space/presentation/mvp/map/MapPresenter.kt
+++ app/src/main/java/com/danilovfa/space/presentation/mvp/map/MapPresenter.kt
@@ -1,5 +1,6 @@
 package com.danilovfa.space.presentation.mvp.map
 
+import com.danilovfa.space.presentation.model.MapMarker
 import com.github.terrakok.cicerone.Router
 import moxy.InjectViewState
 import moxy.MvpPresenter
@@ -8,7 +9,20 @@ import moxy.MvpPresenter
 class MapPresenter(
     private val router: Router
 ) : MvpPresenter<MapView>() {
+
+    private val markers = mutableListOf<MapMarker>()
+    fun getMarkers() = markers.toList()
+
     fun onBackPressed() {
         router.exit()
+    }
+
+    fun deleteMarker(mapMarker: MapMarker): List<MapMarker> {
+        markers.remove(mapMarker)
+        return markers
+    }
+
+    fun addMarker(mapMarker: MapMarker) {
+        markers.add(mapMarker)
     }
 }
